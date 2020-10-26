7ff5bdafdfc03c28f02cf5bdd666ff192db747c0
python-lx <1466446026@qq.com>
2020-10-26 21:07:07 +0800

    代码练习

---
+++ day6.2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        {{ $store.state.num }}
+        <img src="./assets/logo.png">
+        <button @click="add">按钮</button>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    methods: {
+        add() {
+            this.$store.commit('add_num')
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
